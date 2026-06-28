0c674e57178b07adcc16fa9a8a52c8dc3a7bd4d6
Jerin Nadar <jerinnadar@Jerins-MacBook-Air.local>
2026-06-29 00:08:58 +0530

    bg changed

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { heroImage } from "../assets";
 import Deals from "../components/home/Deals";
 import HomeCategories from "../components/home/HomeCategories";
 import PopularProducts from "../components/home/PopularProducts";
@@ -8,7 +9,7 @@ const Home: React.FC = () => {
       <section className="relative overflow-hidden min-h-[390px] mb- rounded flex items-center">
         <img
           className="absolute inset-0 h-full w-full object-cover"
-          src="src/assets/images/webban.png"
+          src={heroImage}
           alt=""
         />
       </section>
